ac829598d8fdfe43f2ba37760d1c4a205bef7e07
Giovanni Miranda <giovannimiranda09@gmail.com>
2025-04-12 18:53:57 -0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,108 @@
 import Image from "next/image";
+import { AiOutlineEye } from "react-icons/ai";
+import { CiCreditCard1, CiSearch } from "react-icons/ci";
+import { FiHelpCircle, FiMessageSquare } from "react-icons/fi";
+import { IoIosArrowForward, IoIosBarcode, IoMdNotificationsOutline } from "react-icons/io";
+import { LiaPiggyBankSolid } from "react-icons/lia";
+import { PiPixLogoThin } from "react-icons/pi";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="w-screen h-screen p-3">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="w-full h-20 flex items-center justify-center gap-4">
+
+        <div className="w-11 h-11 bg-gray-100 rounded-full flex items-center justify-center">
+          GM
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+
+        <div className="w-32 h-11 bg-gray-100 rounded-full flex items-center p-2">
+          <CiSearch className="w-6 h-6" />
+          <input
+            type="text"
+            className="w-full h-full bg-transparent outline-none pl-2"
+            placeholder="Buscar"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+
+        <div className="w-11 h-11 bg-gray-100 rounded-full flex items-center justify-center">
+          <FiHelpCircle className="w-6 h-6" />
+        </div>
+
+        <div className="w-11 h-11 bg-gray-100 rounded-full flex items-center justify-center">
+          <FiMessageSquare className="w-6 h-6" />
+        </div>
+
+        <div className="w-11 h-11 bg-gray-100 rounded-full flex items-center justify-center">
+          <IoMdNotificationsOutline className="w-6 h-6" />
+        </div>
+
+      </div>
+
+      <div className="w-full h-62 flex justify-center">
+        <div className="bg-teal-600 w-96 h-[230px] rounded-2xl px-4 py-3 flex flex-col gap-4">
+
+          <div className="flex justify-between">
+            <p className="text-white text-sm">Conta</p>
+            <p className="text-white text-sm flex items-center">Ver extrato <IoIosArrowForward className="w-4 h-4" /> </p>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <p className="text-white text-2xl">Saldo em conta</p>
+            <div className="bg-teal-500 w-20 h-5"></div>
+          </div>
+
+          <div className="flex justify-between pb-2">
+            <p className="text-white">Rendendo 102% do CDI</p>
+            <AiOutlineEye className="w-6 h-6 text-white" />
+          </div>
+
+          <button className="bg-white w-full h-10 rounded-full flex items-center justify-center">
+            <span className="text-sm font-semibold">Gardar dinheiro</span>
+          </button>
+
+        </div>
+      </div>
+
+      <div className="w-full h-auto flex flex-col gap-3">
+
+        <p className="font-semibold text-xl">Pro dia a dia</p>
+
+        <div className="w-full h-auto flex gap-3">
+
+          <div className="flex flex-col items-center justify-center gap-2">
+            <div className="w-22 h-18 flex items-center justify-center bg-teal-600 rounded-xl">
+              <PiPixLogoThin className="w-8 h-8 text-white" />
+            </div>
+            <p className="text-xs bg-black text-white rounded-2xl px-2 py-1">EM ATÉ 12X</p>
+            <p>Pix</p>
+          </div>
+
+          <div className="flex flex-col items-center gap-2">
+            <div className="w-22 h-18 flex items-center justify-center bg-gray-100 rounded-xl">
+              <IoIosBarcode className="w-8 h-8 text-black" />
+            </div>
+            <p className="text-xs px-2 py-1">Pagar Boleto</p>
+          </div>
+
+          <div className="flex flex-col items-center gap-2">
+            <div className="w-22 h-18 flex items-center justify-center bg-gray-100 rounded-xl">
+              <LiaPiggyBankSolid className="w-8 h-8 text-black" />
+            </div>
+            <p className="text-xs px-2 py-1">Cofrinhos</p>
+          </div>
+
+          <div className="flex flex-col items-center gap-2">
+            <div className="w-22 h-18 flex items-center justify-center bg-gray-100 rounded-xl">
+              <CiCreditCard1 className="w-8 h-8 text-black" />
+            </div>
+            <p className="text-xs px-2 py-1">Cartão virtual</p>
+          </div>
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
